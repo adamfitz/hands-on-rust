c430e57ceb39980694d4a75a9598e3d302fd998d
Adam <adam@networknoob.net>
2024-04-05 09:29:57 +0000

    halfway , dragon is flapping

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -1,29 +1,57 @@
 use bracket_lib::prelude::*;
 
-// The gmae is either in the menu, playing the game or displaying the game over screen
+// The game is either in the menu, playing the game or displaying the game over screen
+
 enum GameMode {
     Menu,
     Playing,
     End
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
+
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5,25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        // TODO: Fill in this stub later
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }       
+        
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
@@ -73,6 +101,60 @@ impl GameState for State {
         }
     }
 }
+
+// Player struct and implementation below here:
+
+struct Player {
+    // x is horizontal co ordinate of the player
+    x: i32,
+    // y is the vertical position of the player
+    y: i32,
+    // velocity represents vertical momentum 
+    velocity: f32
+}
+
+
+impl Player {
+    fn new(x: i32, y:i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        // set is a bracket lib function to 'set' a single character on the screen
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            // this function converts a unicode symbol from this source code to the matching Codepage 437 char
+            to_cp437('@')
+        )
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        // cannot add floating point and integer together, so "as i32" converts to an integer
+        self.y += self.velocity as i32;
+        // the player only moves the dragon up (or down), incrementing x tracks progress through the level
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        // flapping increments the velocity by negative 2 (0 is the top of the screen)
+        self.velocity = -2.0
+    }
+}
+
+
 
 fn main() -> BError {
     // request an 80x50 pixel terminal
